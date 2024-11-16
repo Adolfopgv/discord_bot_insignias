273cc6500e27dedfbe4aa8b6c0db42b4cdd06a3d
Adolfopgv <adolfopgv@gmail.com>
2024-11-16 15:05:17 +0100

    on_ready event finished

--- main.py
+++ main.py
@@ -57,25 +57,30 @@ async def on_ready():
         guild = await BOT.fetch_guild(GUILD_ID) # if guild not in cache
     
     members = [member async for member in guild.fetch_members(limit=None)]
-    users = []
     rows = WORKSHEET_MIEMBROS.get_all_values()
     
-    for index, member in enumerate(members):
-        if rows[1:] != [] and not member.bot and str(member.id) not in [row[0] for row in rows[1:]] and str(member.id) != str(DEV_ID):
-            users.append([str(member.id), member.name, ""])
-                        
-            # borrar usuarios que no estan en la lista
-            
-        elif not member.bot and str(member.id) != str(DEV_ID):
-            users.append([str(member.id), member.name, ""])
+    db_member_ids = [row[0].strip() for row in rows[1:]] if rows[1:] else []
+    users = []
+    members_in_server_ids = []
+    
+    for member in members:
+        if not member.bot and str(member.id) != str(DEV_ID):
+            members_in_server_ids.append(str(member.id).strip())
+            if str(member.id).strip() not in db_member_ids:
+                users.append([str(member.id), member.name, ""])
+                print(f"New member: {member.name} ID: {member.id}")
 
     if users:
         WORKSHEET_MIEMBROS.append_rows(users)
         
-    # for index, row in enumerate(rows[1:] , start=2):
-    #     if row[0] not in str(users[index - 1][0]):
-    #         WORKSHEET_MIEMBROS.delete_rows(index)
-    #         print(f"Deleted row: {index}")
+    rows_to_delete = [
+        index for index, row in enumerate(rows[1:], start=2)
+        if row[0].strip() not in members_in_server_ids
+    ]
+    
+    for index in reversed(rows_to_delete):
+        WORKSHEET_MIEMBROS.delete_rows(index)
+        print(f"Deleted row: {index}")
     
 @BOT.event
 async def on_member_join(member):
@@ -85,13 +90,14 @@ async def on_member_join(member):
     
 @BOT.event
 async def on_member_remove(user):
-    print(f"Member left: Name: {user} ID: {user.id}")
-    rows = WORKSHEET_MIEMBROS.get_all_values()
-    
-    for index, row in enumerate(rows[1:] , start=2):
-        if (row[0].strip() == str(user.id).strip()):
-            WORKSHEET_MIEMBROS.delete_rows(index)
-            print(f"Deleted row: {index}")
-            break
+    if not user.bot:
+        print(f"Member left: Name: {user} ID: {user.id}")
+        rows = WORKSHEET_MIEMBROS.get_all_values()
+        
+        for index, row in enumerate(rows[1:] , start=2):
+            if (row[0].strip() == str(user.id).strip()):
+                WORKSHEET_MIEMBROS.delete_rows(index)
+                print(f"Deleted row: {index}")
+                break
 
 BOT.run(TOKEN)
